00e2001de756a0e2e6f8c47b701f6d0b203a8c0f
Soushi888 <Sacha.pignot@gmail.com>
2020-05-24 14:38:20 -0400

    Create news.php

---
+++ admin/controller/catalog/news.php
@@ -0,0 +1,19 @@
+<?php
+class ControllerCatalogReview extends Controller {
+	private $error = array();
+
+    public function add()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+}
