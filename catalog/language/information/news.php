92074792a388ade0f785a6e662681e8462f7659b
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 20:43:15 -0400

    ss

---
+++ catalog/language/information/news.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title'] = 'Nouvelle';
+
+$_['text_news'] = 'Nouvelle';
+$_['text_title'] = 'Titre';
+$_['description'] = 'Description';
+$_['text_view'] = 'Voir';
+$_['error'] = 'La page vous cherchez est introuvable';
+$_['text_author'] = 'Auteur';
+$_['separator'] = '&gt;';
