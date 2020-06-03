ae04025c354855a29bee640e0afc805aa7e3a3e8
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 21:21:06 -0400

    Add files via upload

--- admin/language/fr-fr/news/news.php
+++ admin/language/fr-fr/news/news.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Nouvelle';
+$_['heading_title'] = 'Nouvelles';
 
 $_['text_news'] = 'Nouvelle';
 $_['text_title'] = 'Titre';
@@ -13,6 +13,7 @@ $_['text_delete'] = 'Supprimer';
 $_['text_yes'] = "Oui";
 $_['text_no'] = "Non";
 $_['text_confirm'] = "Confirmer suppression ?";
+$_['text_link'] = 'Lien';
 $_['text_news_list'] = 'Liste des nouvelles';
 $_['error'] = 'La page vous cherchez est introuvable';
 $_['separator'] = '&gt;';
