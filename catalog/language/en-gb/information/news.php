ec1cf15643894bb3febed25a4d11e4efade09a6d
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 20:39:31 -0400

    ssj

--- catalog/language/en-gb/information/news.php
+++ catalog/language/en-gb/information/news.php
@@ -7,4 +7,5 @@ $_['text_title'] = 'Title';
 $_['description'] = 'Description';
 $_['text_view'] = 'View';
 $_['error'] = 'The page you are looking for cannot be found.';
+$_['text_author'] = 'Author';
 $_['separator'] = '&gt;';
