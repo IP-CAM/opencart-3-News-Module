a6a8b9836adb7e1627427f64b89d14248d821117
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 21:21:27 -0400

    Add files via upload

--- admin/language/en-gb/news/news.php
+++ admin/language/en-gb/news/news.php
@@ -2,7 +2,7 @@
 // Heading
 $_['heading_title'] = 'News';
 
-$_['text_news'] = 'News';
+$_['text_news'] = 'New';
 $_['text_title'] = 'Title';
 $_['text_News'] = 'Description';
 $_['text_author'] = 'Author';
@@ -13,6 +13,7 @@ $_['text_delete'] = 'Delete';
 $_['text_yes'] = "Yes";
 $_['text_no'] = "No";
 $_['text_confirm'] = "Confirm Delete ?";
+$_['text_link'] = 'Link';
 $_['text_news_list'] = 'News list';
 $_['error'] = 'The page you are looking for cannot be found.';
 $_['separator'] = '&gt;';
