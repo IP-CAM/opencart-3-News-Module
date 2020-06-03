d5a5bd4d1052bffa8f61443077695fbc47e49b45
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 21:23:14 -0400

    Add files via upload

--- catalog/language/en-gb/information/news.php
+++ catalog/language/en-gb/information/news.php
@@ -2,10 +2,11 @@
 // Heading
 $_['heading_title'] = 'News';
 
-$_['text_news'] = 'News';
+$_['text_news'] = 'New';
 $_['text_title'] = 'Title';
 $_['description'] = 'Description';
 $_['text_view'] = 'View';
+$_['text_link'] = 'Link';
 $_['error'] = 'The page you are looking for cannot be found.';
 $_['text_author'] = 'Author';
 $_['separator'] = '&gt;';
