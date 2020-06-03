77ae7e4581f9193699e2d9e0845b7c75bb17fb2d
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 20:26:17 -0400

    Add files via upload

---
+++ admin/language/news/news.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title'] = 'Nouvelle';
+
+$_['text_news'] = 'Nouvelle';
+$_['text_title'] = 'Titre';
+$_['text_News'] = 'Description';
+$_['text_author'] = 'Auteur';
+$_['text_create'] = 'Ajouter';
+$_['text_update'] = 'Modifier';
+$_['text_submit'] = 'Envoyer !';
+$_['text_delete'] = 'Supprimer';
+$_['text_news_list'] = 'Liste des nouvelles';
+$_['error'] = 'La page vous cherchez est introuvable';
+$_['separator'] = '&gt;';
