ecc8cb07123470fc07c199d9b5b5c3d27f07c0ec
MI7QC <54377301+MI7QC@users.noreply.github.com>
2020-06-02 21:23:44 -0400

    Add files via upload

--- catalog/language/fr-fr/information/news.php
+++ catalog/language/fr-fr/information/news.php
@@ -1,11 +1,12 @@
 <?php
 // Heading
-$_['heading_title'] = 'Nouvelle';
+$_['heading_title'] = 'Nouvelles';
 
 $_['text_news'] = 'Nouvelle';
 $_['text_title'] = 'Titre';
 $_['description'] = 'Description';
 $_['text_view'] = 'Voir';
+$_['text_link'] = 'Lien';
 $_['error'] = 'La page vous cherchez est introuvable';
 $_['text_author'] = 'Auteur';
 $_['separator'] = '&gt;';
